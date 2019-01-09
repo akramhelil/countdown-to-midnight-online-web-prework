2bfed48db52cfad9c36506cbc61da0e7cedddd0f
Akram Helil <akramhelil@gmail.com>
2019-01-09 20:04:29 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,22 +1,19 @@
 #write your code here
 
 def countdown(newyear)
-  
+
   while newyear > 0
     puts "#{newyear} SECOND(S)!"
     newyear -= 1
   end
-  "HAPPY NEW YEAR!"  
+  "HAPPY NEW YEAR!"
 end
 
 def countdown(newyear)
-  
-  while newyear > 0
+    while newyear > 0
     puts "#{newyear} SECOND(S)!"
     sleep(1)
     newyear -= 1
-  
   end
-  "HAPPY NEW YEAR!"  
+  "HAPPY NEW YEAR!"
 end
-

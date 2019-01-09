48141a71a601df03e2d3ce398d21c84dac638bba
Akram Helil <akramhelil@gmail.com>
2019-01-09 20:03:36 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
 
-def countdown
+def countdown(newyear)
+  
+  while newyear > 0
+    puts "#{newyear} SECOND(S)!"
+    newyear -= 1
+  end
+  "HAPPY NEW YEAR!"  
 end
+
+def countdown(newyear)
+  
+  while newyear > 0
+    puts "#{newyear} SECOND(S)!"
+    sleep(1)
+    newyear -= 1
+  
+  end
+  "HAPPY NEW YEAR!"  
+end
+
